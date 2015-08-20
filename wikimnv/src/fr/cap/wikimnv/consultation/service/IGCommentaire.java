e0bded841012701eef9cdfed0a506136a08b26e7
sohailyaqub <contact@sohail.fr>
2015-08-20 16:36:53 +0200

    Mise à jours commentaire

--- wikimnv/src/fr/cap/wikimnv/consultation/service/IGCommentaire.java
+++ wikimnv/src/fr/cap/wikimnv/consultation/service/IGCommentaire.java
@@ -2,17 +2,12 @@ package fr.cap.wikimnv.consultation.service;
 
 import java.util.Set;
 
-
-
-
-
-import fr.cap.wikimnv.consultation.domaine.pojo.Commentaire;
-
-
+import fr.cap.wikimnv.domain.pojo.Commentaire;
+import fr.cap.wikimnv.domain.pojo.EtatPublication;
 public interface IGCommentaire {
 	Set<Commentaire> lister() ;
 	Commentaire lire(int id) ;
-	int sauver(Commentaire commentaire) ;
-	int supprimer(Commentaire commentaire) ;
-	boolean banir(boolean etat) ;
+	Commentaire sauver(Commentaire commentaire) ;
+	Commentaire supprimer(Commentaire commentaire) ;
+	Commentaire banir(Commentaire commentaire,  EtatPublication etat) ;
 	}
