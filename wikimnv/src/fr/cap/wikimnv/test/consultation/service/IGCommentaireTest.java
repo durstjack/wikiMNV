0b25ef5351c7c4694d6b6c7ed798256ed747f3e2
messan55 <messan.gnemegna@gmail.com>
2015-08-21 09:32:40 +0100

    test iCom

---
+++ wikimnv/src/fr/cap/wikimnv/test/consultation/service/IGCommentaireTest.java
@@ -0,0 +1,34 @@
+package fr.cap.wikimnv.test.consultation.service;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class IGCommentaireTest {
+
+	@Test
+	public void testLister() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testLire() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSauver() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSupprimer() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testBanir() {
+		fail("Not yet implemented");
+	}
+
+}
